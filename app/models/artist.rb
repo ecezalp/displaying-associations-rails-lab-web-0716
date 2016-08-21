3b18bd0befe3a1c121377877fe9659ecb2f93a58
Ece Ozalp <github email address>
2016-08-21 15:43:54 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+
+  def song_count
+    song_count = self.songs.count
+    song_count
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    artist_name = self.artist.name
+    artist_name
+  end
+
+  def styled_display
+    "#{artist_name.titleize} - #{self.title.titleize}"
+  end
 end
